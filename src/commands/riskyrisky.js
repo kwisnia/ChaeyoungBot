e5ca3469b41b021e128de10ecf9dda6e38dafd7b
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2020-12-23 21:05:10 +0100

    Add riskyrisky

---
+++ src/commands/riskyrisky.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "riskyrisky",
+  description: "https://www.youtube.com/watch?v=_xtJWYcBsoY",
+  execute(message, args) {
+    message.channel.send(
+      ":oncoming_police_car: risky risky wiggy wiggy this is an emergency :oncoming_police_car:"
+    );
+  },
+};
