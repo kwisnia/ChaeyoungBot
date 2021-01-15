86d119c805c9bdd805d5eca93646e64925eb46b5
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2021-01-15 17:58:43 +0100

    dodana najwazniejsza komenda w dniu 15.01

---
+++ src/commands/cojest.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "cojest",
+    description: "Co jest kurwa",
+        execute(message, args) {
+          message.channel.send(
+            "https://media.discordapp.net/attachments/694564497662148679/799683585870266399/unknown.png"
+          );
+        },
+      };
+  
