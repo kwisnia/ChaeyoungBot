9dd05fb9a7ee9eb66f9ccb34d1d70f5841d7395a
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2021-03-22 16:05:53 +0100

    bo kacper

---
+++ src/boja.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "boja",
+    description: "Bo Kacper tak robi",
+    execute(message, args) {
+      message.channel.send(
+        "https://media.discordapp.net/attachments/644608604959408156/823572844670746654/generalizuj.png"
+      );
+    },
+  };
+  
