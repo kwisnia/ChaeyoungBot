7e18e7da603deb2bd3e4e1fac503dbb7669987ec
Kacper Wiśniewski <61986363+kwisnia@users.noreply.github.com>
2021-03-23 12:32:39 +0000

    Add aliases and reloading

--- src/index.js
+++ src/index.js
@@ -100,11 +100,13 @@ function gotMessage(msg) {
   reactIdol(msg);
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   const args = msg.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(command)) return;
+  const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
   try {
-    client.commands.get(command).execute(msg, args);
+    command.execute(msg, args);
   } catch (error) {
     console.error(error);
     msg.reply("there was an error trying to execute that command!");
