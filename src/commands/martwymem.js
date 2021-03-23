e7bd9373b47d2c521bde846b05a253b1d4f0a70d
Kacper Wiśniewski <61986363+kwisnia@users.noreply.github.com>
2021-03-23 14:40:44 +0000

    Fixes

--- src/commands/martwymem.js
+++ src/commands/martwymem.js
@@ -7,26 +7,26 @@ module.exports = {
   execute(message, args) {
     let memes = fs
       .readFileSync("./src/commands/memy.txt", "utf8")
-      .split(/\r\n/g);
-    console.log(memes);
+      .split(/\n/g);
+    console.log(memes.length);
     if (args[0] === "dodaj") {
       if (!args[1]) return message.channel.send("No ale co dodać");
       memes.push(args[1]);
-      fs.appendFileSync("./src/commands/memy.txt", `\r\n${args[1]}`);
-      message.channel.send(`Dodano ${args[1]}!`);
+      fs.appendFileSync("./src/commands/memy.txt", `\n${args.slice(1).join(' ')}`);
+      message.channel.send(`Dodano ${args.slice(1).join(' ')}!`);
       return;
     } else if (args[0] === "usun") {
       if (!args[1]) return message.channel.send("No ale co usunąć");
-      memes = memes.filter((mem) => args[1] !== mem);
-      fs.writeFileSync("./src/commands/memy.txt", memes.join("\r\n"));
-      message.channel.send(`Usunięto ${args[1]}`);
+      memes = memes.filter((mem) => args.slice(1).join(' ') !== mem);
+      fs.writeFileSync("./src/commands/memy.txt", memes.join("\n"));
+      message.channel.send(`Usunięto ${args.slice(1).join(' ')}`);
     } else if (args[0] === "pobierz") {
       const memeOfTheDay =
         memes[Math.floor(Math.random() * Math.floor(memes.length))];
       console.log(memeOfTheDay);
       return memeOfTheDay;
     } else if (args[0] === "wyswietl") {
-        message.channel.send(`\`\`\`${memes}\`\`\``)
+        message.channel.send(`\`\`\`${memes.join('\n')}\`\`\``)
     } else {
       return message.channel.send("Incorrect argument");
     }
