4c510d0617ab4b1bc99bb23015aefee1ff17eb84
kwisnia <230037@edu.p.lodz.pl>
2021-09-15 21:11:32 +0200

    Love handler fixes

--- src/services/message/reactions/LoveHandler.ts
+++ src/services/message/reactions/LoveHandler.ts
@@ -1,17 +1,21 @@
+import { userMention } from '@discordjs/builders';
 import { Message } from 'discord.js';
 import { inject, injectable } from 'inversify';
 import { Logger } from 'winston';
 import BOT_TYPES from '../../../botTypes';
 import { IReactHandler } from './IReactHandler';
 
+const BOT_ID = '791286898123079680';
 @injectable()
 class LoveHandler implements IReactHandler {
   constructor(@inject(BOT_TYPES.Logger) private logger: Logger) {}
 
   public check(message: Message): boolean {
     if (
-      message.mentions.has('881494881049137152') &&
-      message.content.includes('❤️')
+      message.mentions.has(BOT_ID) ||
+      (message.type === 'REPLY' &&
+        message.mentions.repliedUser!.id === BOT_ID &&
+        message.content.includes('❤️'))
     ) {
       this.logger.info('<3');
       return true;
@@ -22,7 +26,9 @@ class LoveHandler implements IReactHandler {
   public async react(message: Message): Promise<void> {
     this.logger.info('Love reaction called');
     await message.reply(
-      `${message.author} https://tenor.com/view/chaeyoung-son-chaeyoung-twice-cute-gif-19442356`,
+      `${userMention(
+        message.author.id,
+      )} https://tenor.com/view/chaeyoung-son-chaeyoung-twice-cute-gif-19442356`,
     );
     return Promise.resolve();
   }
